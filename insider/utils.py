6d2effafdc882b95bf3724e626a6f254aef1c110
Kaiqi <kaiqi@kth.se>
2020-05-21 22:21:50 +0200

    Add surge limit calculation

--- insider/utils.py
+++ insider/utils.py
@@ -4,3 +4,9 @@ import plotly.graph_objects as go
 def set_layout():
     layout = go.Layout(xaxis=dict(type="category", tickangle=270))
     return layout
+
+
+def calculate_surged_limit(p):
+    """Calculate the price if upper surged limit is reached."""
+    surged = round(p * 1.1, 2)
+    return surged
