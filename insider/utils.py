e58f62feab889c27ebd42573980a1ab5fdd03ff6
Kaiqi Dong <kaiqi@kth.se>
2020-05-23 19:40:08 +0200

    add surge limit (#22)

--- insider/utils.py
+++ insider/utils.py
@@ -10,3 +10,20 @@ def calculate_surged_limit(p):
     """Calculate the price if upper surged limit is reached."""
     surged = round(p * 1.1, 2)
     return surged
+
+
+def reach_surged_limit(
+    df, year_month_day: str, preprice_col: str = "preprice", close_col: str = "price"
+):
+    """Check"""
+    df["calculated_surged_price"] = df[preprice_col].apply(
+        lambda x: calculate_surged_limit(x)
+    )
+    df[year_month_day] = (
+        (df["calculated_surged_price"] == df[close_col])
+        & (df[preprice_col] != df[close_col])
+    ).astype(int)
+
+    cols = ["code", "name"]
+    cols = cols.append(year_month_day)
+    return df[cols]
